37a91034b649af22607d5a132cf55ecb5a03e759
GennadyBr <gennady.budazhapov@gmail.com>
2023-12-26 13:53:48 +0300

    nginx conf

--- start.sh
+++ start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 sudo docker network create nginx_proxy
-sudo docker-compose up -d --build
+sudo docker compose up -d --build
+#sudo docker-compose up -d --build
 sudo docker logs nginx_proxy
-#
+
+#сделать исполняемым
+#chmod +x start.sh
+
+#закрыть Apache2 на 80 порту
+#service apache2 stop
+
