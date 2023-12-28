46f8338ee5a9f6f53b7ee6348b07444f0c18b758
GennadyBr <gennady.budazhapov@gmail.com>
2023-12-28 12:16:38 +0300

    flask_mongo_app

--- start.sh
+++ start.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+#закрыть Apache2 на 80 порту
+#service apache2 stop
+
 sudo docker network create nginx_proxy
+sudo docker compose down
 sudo docker compose up -d --build
 #sudo docker-compose up -d --build
 sudo docker logs nginx_proxy
 
+sudo docker network inspect nginx_proxy
+
 #сделать исполняемым
 #chmod +x start.sh
-
-#закрыть Apache2 на 80 порту
-#service apache2 stop
-
